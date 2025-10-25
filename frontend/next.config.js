f1d7c686b906993a8c7d92f44f361880ab128c51
Kunal Badole <kunalbadole5@gmail.com>
2025-10-25 17:36:41 +0530

    seo changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,28 @@ const nextConfig = {
     unoptimized: true,
     domains: ['clicksummary.com', 'lh3.googleusercontent.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+      {
+        source: '/robots.txt',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
