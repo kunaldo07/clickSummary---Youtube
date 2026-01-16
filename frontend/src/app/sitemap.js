73c7cbb49777e0d785144442d03085c040812473
Kunal Badole <kunalbadole5@gmail.com>
2026-01-16 16:51:53 +0530

    sitemap updated

--- frontend/src/app/sitemap.js
+++ frontend/src/app/sitemap.js
@@ -14,6 +14,12 @@ export default function sitemap() {
       priority: 0.9,
     },
     {
+      url: 'https://clicksummary.com/reddit/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
       url: 'https://clicksummary.com/privacy/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
